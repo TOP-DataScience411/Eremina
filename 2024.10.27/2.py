029282fd8fcbdfd9fd8278930d8069165bc0eab5
EDV-1414 <dindin1414@mail.ru>
2024-11-06 13:06:52 +0700

    Update 2.py

--- 2024.10.27/2.py
+++ 2024.10.27/2.py
@@ -1,1 +1,9 @@
-print()
+chislo=input('Введите целое число: ')
+chislo_sled=int(chislo)+1
+chislo_pred=int(chislo)-1
+print("Следующее за числом ",chislo," число:",chislo_sled,'\n',"Для числа ",chislo," предыдущее число: ",chislo_pred,sep='')
+
+# C:\Users\Dina>python "D:\!   ___ОБУЧЕНИЕ DS\_ЗАДАНИЯ_DS\Задание 1\2.py"
+# Введите целое число: 20
+# Следующее за числом 20 число:21
+# Для числа 20 предыдущее число: 19
