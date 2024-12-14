6bbfe86eafaa9fb52173261896271e8801764665
Gennadiy <renardes@gmail.com>
2024-12-14 20:16:53 +0500

    2024.11.09: образец для задачи 6

---
+++ 2024.11.09/# ref 6.py
@@ -0,0 +1,11 @@
+inp = input('ввод: ')
+answer = 'нет'
+
+prefix = inp[:2]
+if prefix != '1b':
+    if set(prefix) < set('01b')
+        if set(inp[2:]) <= set('01')
+            answer = 'да'
+
+print(answer)
+
